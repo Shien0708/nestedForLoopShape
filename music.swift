8e3fead73edfdc94b7897a0e0832e094a04f45f6
Shien0708 <93871017+Shien0708@users.noreply.github.com>
2022-02-01 19:54:47 +0800

    Create music.swift

---
+++ music.swift
@@ -0,0 +1,13 @@
+import Foundation
+import UIKit
+
+struct Music {
+    var name: String
+    var image: String
+}
+
+let myMusic = [
+    Music(name: "I'm the One", image: "ImTheOne"),
+Music(name: "Let Me Love You", image: "LetMeLoveYou"),
+Music(name: "Shape of You", image: "shapeOfYou"),
+Music(name: "Closer", image: "closer")]
